f81c0f43cd708862476f4c22d7f25b9bf5c14111
hudzaabdr <hudzaabdurrahmanmail@gmail.com>
2026-04-21 21:47:26 +0700

    Update tes.js

--- tes.js
+++ tes.js
@@ -1,11 +1,1 @@
-<script>
-function startCamera() {
-  navigator.mediaDevices.getUserMedia({ video: true })
-    .then(function(stream) {
-      alert("Kamera berhasil diakses!");
-    })
-    .catch(function(err) {
-      alert("Kamera ditolak atau tidak tersedia");
-    });
-}
-</script>
+
